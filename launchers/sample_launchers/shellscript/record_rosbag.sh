c75601a855049c36286cee09dd0764e0dec7e858
Yuki Akimoto <yuki_akimoto@jp.honda>
2025-07-28 15:42:27 +0900

    Modify `record_rosbag.sh`.

--- launchers/sample_launchers/shellscript/record_rosbag.sh
+++ launchers/sample_launchers/shellscript/record_rosbag.sh
@@ -2,15 +2,10 @@
 << COMMENTOUT
 <Usage Example>
 record_rosbag.sh
-record_rosbag.sh 
+record_rosbag.sh zed_image_data
 COMMENTOUT
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-topic_list_yaml_path="${SCRIPT_DIR}/../config/topic_list.yaml"
-read_yaml() {
-    node_path=$1
-    python3 -c "import yaml; print(yaml.safe_load(open('$topic_list_yaml_path'))${node_path})" 2>/dev/null
-}
 
 bag_name=${1:-test}
 
@@ -19,11 +14,11 @@ bag_dir="${HOME}/rosbag/${date_str}"
 mkdir -p ${bag_dir}
 
 ros2 bag record -o "${bag_dir}"/"${bag_name}" \
-    --qos-profile-overrides-path ${SCRIPT_DIR}/../config/qos_setting.yaml \
-    $(read_yaml "['sensing']['zedx']['left_image']['undistorted']") \
-    $(read_yaml "['sensing']['zedx']['imu']") \
-    $(read_yaml "['sensing']['input_can_data']") \
-    $(read_yaml "['sensing']['odometry']['gyro']") \
-    $(read_yaml "['sensing']['rear_potentiometer']") \
+    --qos-profile-overrides-path ${SCRIPT_DIR}/../../../common/aiformula_interfaces/config/qos_setting.yaml \
+    /aiformula_sensing/zed_node/left_image/undistorted \
+    /aiformula_sensing/zed_node/imu \
+    /aiformula_sensing/vehicle_info \
+    /aiformula_sensing/gyro_odometry_publisher/odom \
+    /aiformula_sensing/rear_potentiometer/yaw \
     /tf \
     /tf_static
